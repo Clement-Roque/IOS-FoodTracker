b431f7fcc764e9c9c6ce4505257769ec5285a763
Clément Roque <clement@tarmactechnologies.com>
2021-02-10 08:57:18 +0100

    add image picking

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -31,7 +31,21 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     
     //MARK: UIImagePickerControllerDelegate
     
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
     
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        guard let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage
+        else {
+            fatalError("Expected a dictionary containing an image, but was provided the following: \(info)")
+        }
+        
+        photoImageView.image = selectedImage
+        
+        dismiss(animated: true, completion: nil)
+    }
     
     //MARK: Actions
 
